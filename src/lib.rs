4425412381595032c32ca4fef05918a090fdccfe
Dom Twitty <dtwitty@linkedin.com>
2025-01-07 11:45:51 -0800

    Look Ma, no Regex!

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use nom::bytes::complete::{tag, take_till1};
 use nom::combinator::{map, value};
 use nom::error::ErrorKind;
 use nom::multi::many0;
-use regex::Regex;
 
 #[derive(Debug, Clone)]
 enum Token<'a> {
@@ -27,44 +26,104 @@ fn lex(s: &str) -> Vec<Token> {
     .1
 }
 
-fn to_regex(tokens: Vec<Token>) -> Regex {
-    let mut s = String::new();
-    s.push('^');
-    for t in tokens {
-        match t {
-            Token::Literal(l) => s.push_str(regex::escape(l).as_str()),
-            Token::Any => s.push_str(".*"),
-            Token::Single => s.push_str("."),
+fn normalize(tokens: &mut Vec<Token>) {
+    // We just want to fix the case of "%_", transforming it into "_%".
+    // This way, we can just skip a single character instead of doing a complicated match.
+    for i in 0..tokens.len() - 1 {
+        if let (Token::Any, Token::Single) = (&tokens[i], &tokens[i + 1]) {
+            tokens[i] = Token::Single;
+            tokens[i + 1] = Token::Any;
         }
     }
-    s.push('$');
-    Regex::new(&s).unwrap()
 }
 
 #[derive(Debug)]
-enum LikeMatcher {
-    /// Matches the empty string.
-    Empty,
-
-    Regex(Regex),
+struct LikeMatcher<'a> {
+    tokens: Vec<Token<'a>>,
 }
 
-impl LikeMatcher {
+impl<'a> LikeMatcher<'a> {
     pub fn new(s: &str) -> LikeMatcher {
-        if s.is_empty() {
-            return LikeMatcher {
-                regex: Regex::new("^$").unwrap(),
-            };
-        }
-
-        let tokens = lex(s);
-        let regex = to_regex(tokens);
-
-        LikeMatcher { regex }
+        let mut tokens = lex(s);
+        normalize(&mut tokens);
+        LikeMatcher { tokens }
     }
 
-    pub fn matches(&self, s: &str) -> bool {
-        self.regex.is_match(s)
+    pub fn matches(&self, input: &str) -> bool {
+        // The index into the list of tokens.
+        let mut t = 0;
+
+        // The index into the string.
+        let mut s = 0;
+
+        while t < self.tokens.len() {
+            if t == self.tokens.len() - 1 {
+                // This is the last token.
+                return match self.tokens[t] {
+                    Token::Any => {
+                        // We can match anything that's left. We're done.
+                        true
+                    }
+
+                    Token::Single => {
+                        // We need to match a single character at the end.
+                        // This is equivalent to saying "we have 1 character left".
+                        s == input.len() - 1
+                    }
+
+                    Token::Literal(literal) => {
+                        // Check whether the remaining input matches the literal.
+                        &input[s..] == literal
+                    }
+                };
+            }
+
+            // We have at least 2 tokens left.
+            match (&self.tokens[t], &self.tokens[t + 1]) {
+                (Token::Any, Token::Any) => {
+                    // We can just skip the first Any token.
+                    t += 1;
+                }
+
+                (Token::Any, Token::Single) => {
+                    unreachable!("Normalization should have caught Any followed by Single.");
+                }
+
+                (Token::Any, Token::Literal(literal)) => {
+                    // Skip to the next literal.
+                    if let Some(x) = input[s..].find(literal) {
+                        // We found the literal. Skip over it and both tokens.
+                        s += x + literal.len();
+                        t += 2;
+                    } else {
+                        // We did not find the literal.
+                        return false;
+                    }
+                }
+
+                (Token::Single, _) => {
+                    // Skip over the single character.
+                    // If there isn't one, the loop condition will catch it.
+                    s += 1;
+                    t += 1;
+                }
+
+                (Token::Literal(literal), _) => {
+                    if input[s..].starts_with(literal) {
+                        // We found the literal. Skip over it.
+                        s += literal.len();
+                        t += 1;
+                    } else {
+                        // We did not find the literal.
+                        return false;
+                    }
+                }
+            }
+        }
+
+        let tokens_done = t == self.tokens.len();
+        let input_done = s == input.len();
+        tokens_done && input_done
     }
 }
 
